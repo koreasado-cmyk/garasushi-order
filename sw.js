80eb8523fa0899f4d9f586b1655f019b6b1a39f1
koreasado-cmyk <koreasado@gmail.com>
2026-05-29 18:59:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 /* 가라스시 발주 - Service Worker
- * Build: 20260529-1851
+ * Build: 20260529-1856
  * 전략: network-first → 캐시 폴백
  */
-const CACHE = 'garasushi-order-v20260529-1851';
+const CACHE = 'garasushi-order-v20260529-1856';
 const CORE = [
   './',
   './index.html',
